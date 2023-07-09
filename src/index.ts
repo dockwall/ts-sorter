0faa495a595a12980b74cf4f223c84dfcca2f550
Stas Leshuk <leshuksu@gmail.com>
2023-07-09 19:10:19 +0300

    optimized bubble sorting

--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,18 @@ class Sorter {
     const { length: collectionLength } = this.collection;
 
     for (let i = 0; i < collectionLength; i++) {
+      let isSwapped = false;
+
       for (let j = 0; j < collectionLength - i - 1; j++) {
         if (this.collection[j] > this.collection[j + 1]) {
           const temp = this.collection[j];
           this.collection[j] = this.collection[j + 1];
           this.collection[j + 1] = temp;
+          isSwapped = true;
         }
       }
+
+      if (!isSwapped) break;
     }
   }
 }
