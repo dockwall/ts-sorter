243e4149cda58538930888927bd906cc5e17ce22
Stanislav Leshuk <leshuksu@gmail.com>
2023-07-09 22:16:23 +0300

    1 array bubble sorter (#1)
    * scaffolded sorter class (only arrays)
    
    * implemented sort method (bubble)
    
    * optimized bubble sorting

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log("hello");
+class Sorter {
+  collection: number[];
+
+  constructor(collection: number[]) {
+    this.collection = collection;
+  }
+
+  sort(): void {
+    const { length: collectionLength } = this.collection;
+
+    for (let i = 0; i < collectionLength; i++) {
+      let isSwapped = false;
+
+      for (let j = 0; j < collectionLength - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const temp = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = temp;
+          isSwapped = true;
+        }
+      }
+
+      if (!isSwapped) break;
+    }
+  }
+}
+
+const sorter = new Sorter([10, 3, -5, 0, 1]);
+console.log("Collection before sort:", sorter.collection);
+sorter.sort();
+console.log("Collection after sort:", sorter.collection);
