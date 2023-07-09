6cdaea2fd852bb004a33cb25f82e1a3fcc2cd2a9
Stas Leshuk <leshuksu@gmail.com>
2023-07-09 19:07:40 +0300

    implemented sort method (bubble)

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,22 @@ class Sorter {
     this.collection = collection;
   }
 
-  sort(): void {}
+  sort(): void {
+    const { length: collectionLength } = this.collection;
+
+    for (let i = 0; i < collectionLength; i++) {
+      for (let j = 0; j < collectionLength - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const temp = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = temp;
+        }
+      }
+    }
+  }
 }
 
 const sorter = new Sorter([10, 3, -5, 0, 1]);
+console.log("Collection before sort:", sorter.collection);
 sorter.sort();
-console.log(sorter.collection);
+console.log("Collection after sort:", sorter.collection);
