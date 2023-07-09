41bbb4cb59271ed14c0858fa3d94588a52b537d3
Stas Leshuk <leshuksu@gmail.com>
2023-07-09 18:39:37 +0300

    scaffolded sorter class (only arrays)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello");
+class Sorter {
+  collection: number[];
+
+  constructor(collection: number[]) {
+    this.collection = collection;
+  }
+
+  sort(): void {}
+}
+
+const sorter = new Sorter([10, 3, -5, 0, 1]);
+sorter.sort();
+console.log(sorter.collection);
