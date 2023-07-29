cf06ca2565076f050fd0acfacef1ad19a245d6ca
Stas Leshuk <leshuksu@gmail.com>
2023-07-29 22:34:00 +0300

    added linked list (numbers) sorting

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import { Sorter } from "./Sorter";
 import { NumbersArrayCollection } from "./NumbersArrayCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedListCollection } from "./LinkedListCollection";
 
 const numbersCollection = new NumbersArrayCollection([50, 10, 3, -5, 0, 1]);
 const charactersCollection = new CharactersCollection("zABCDoÖOZabcd");
+const linkedListCollection = new LinkedListCollection();
+linkedListCollection.push(50);
+linkedListCollection.push(10);
+linkedListCollection.push(3);
+linkedListCollection.push(-5);
+linkedListCollection.push(0);
+linkedListCollection.push(1);
 
 console.log("\nnumber[] bubble sorting");
 const numbersArraySorter = new Sorter(numbersCollection);
@@ -16,3 +24,11 @@ const characterSorter = new Sorter(charactersCollection);
 console.log("string before sort:", charactersCollection.data);
 characterSorter.bubbleSort();
 console.log("string after sort:", charactersCollection.data);
+
+console.log("\nlinked list bubble sorting");
+const linkedListSorter = new Sorter(linkedListCollection);
+console.log("linked list before sort:");
+linkedListCollection.print();
+linkedListSorter.bubbleSort();
+console.log("linked list after sort:");
+linkedListCollection.print();
